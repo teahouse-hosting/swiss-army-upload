b8cbaf7e49c0cba3f0c479139a2643e478137e61
Jamie Bliss <jamie@ivyleav.es>
2026-02-23 17:09:25 -0500

    Tweak sphinx conf

--- docs/conf.py
+++ docs/conf.py
@@ -10,6 +10,9 @@ project = "Swiss Army Upload"
 copyright = "2026, Teahouse Hosting"
 author = "Teahouse Hosting"
 
+primary_domain = None
+highlight_language = None
+
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
@@ -28,3 +31,8 @@ html_theme = "sphinx_rtd_theme"
 html_static_path = ["_static"]
 html_logo = "logo-simple.svg"
 # html_baseurl = ...
+
+html_theme_options = {
+    "style_nav_header_background": "#6B2E57ff",
+    # TODO: Link sources to codeberg
+}
