7079c522f3980f09a85b3a2c0e2fff9870251bd1
Jamie Bliss <jamie@ivyleav.es>
2026-03-30 16:04:28 -0400

    Add a large file test

--- tests/test_single_file.py
+++ tests/test_single_file.py
@@ -2,6 +2,8 @@
 Generic tests on single files
 """
 
+import random
+
 import pytest
 
 
@@ -24,7 +26,30 @@ async def test_round_trip(keyring, sau_cli, remote_url, tmp_path):
     (tmp_path / "test.txt").write_text(data)
     await sau_cli(["put", tmp_path / "test.txt", remote_url], check=True)
     await sau_cli(["get", remote_url, tmp_path / "download.txt"], check=True)
-    assert (tmp_path / "test.txt").read_text() == data
+    assert (tmp_path / "download.txt").read_text() == data
+
+
+@pytest.mark.parametrize(
+    "remote_url",
+    [
+        "tea://clarice.teahouse/file.txt",
+        # "pages://clarice.gitpages/file.txt",
+    ],
+)
+async def test_round_trip_large(keyring, sau_cli, remote_url, tmp_path):
+    await keyring.set_password(
+        "counter.teahouse.cafe", "alice@valid.test", "sweet little angel"
+    )
+    with (tmp_path / "large.bin").open("wb") as fobj:
+        # Write out 10MB
+        for _ in range(1_000):
+            fobj.write(random.randbytes(10_000))
+
+    await sau_cli(["put", tmp_path / "large.bin", remote_url], check=True)
+    await sau_cli(["get", remote_url, tmp_path / "download.bin"], check=True)
+    assert (tmp_path / "large.bin").read_bytes() == (
+        tmp_path / "download.bin"
+    ).read_bytes()
 
 
 @pytest.mark.parametrize(
