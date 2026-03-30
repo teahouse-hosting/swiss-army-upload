35ab251fee24ab1deeaa3fa5571cfb49bda68b7e
Jamie Bliss <jamie@ivyleav.es>
2026-03-30 15:55:08 -0400

    Mix up test data

--- tests/test_single_file.py
+++ tests/test_single_file.py
@@ -30,8 +30,8 @@ async def test_round_trip(keyring, sau_cli, remote_url, tmp_path):
 @pytest.mark.parametrize(
     "upload_url,result_url",
     [
-        ("tea://alice.teahouse/file.txt", "https://alice.teahouse/file.txt"),
-        # ("pages://alice.gitpages/file.txt", "https://alice.gitpages/file.txt"),
+        ("tea://beth.teahouse/file.txt", "https://beth.teahouse/file.txt"),
+        # ("pages://beth.gitpages/file.txt", "https://beth.gitpages/file.txt"),
     ],
 )
 async def test_publishes(
@@ -40,13 +40,9 @@ async def test_publishes(
     await keyring.set_password(
         "counter.teahouse.cafe", "alice@valid.test", "sweet little angel"
     )
-    data = (
-        "According to all known laws of aviation, there is no way a bee should be able to fly. "
-        "Its wings are too small to get its fat little body off the ground. "
-        "The bee, of course, flies anyway because bees don't care what humans think is impossible."
-    )
-    (tmp_path / "test.txt").write_text(data)
-    await sau_cli(["put", tmp_path / "test.txt", upload_url], check=True)
+    data = "Hope is a discipline. It requires that you not give in to despair. So I'm here to tell you: don't despair."
+    (tmp_path / "published.txt").write_text(data)
+    await sau_cli(["put", tmp_path / "published.txt", upload_url], check=True)
 
     resp = await http_client.get(result_url)
     resp.raise_for_status()
