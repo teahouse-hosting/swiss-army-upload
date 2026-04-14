78a78e3e55714d72ab1d40cdbd8cc8b9e83cca42
Jamie Bliss <jamie@ivyleav.es>
2026-04-14 16:47:33 -0400

    Write a test for sync

---
+++ tests/test_sync.py
@@ -0,0 +1,23 @@
+from pathlib import Path
+import pytest
+
+
+PROJECT = Path(__file__).absolute().parent.parent
+
+
+@pytest.mark.parametrize(
+    "remote_url",
+    [
+        "tea://grace.teahouse/",
+        # "pages://alice.gitpages/file.txt",
+    ],
+)
+async def test_round_trip(sau_cli, tmp_path, remote_url, use_good_oidc):
+    await sau_cli(["sync", str(PROJECT / "test-site"), remote_url], check=True)
+    await sau_cli(["sync", remote_url, tmp_path], check=True)
+
+    for src_file in (PROJECT / "test-site").iterdir():
+        if not src_file.is_file():
+            continue
+        dest_file = tmp_path / src_file.relative_to(PROJECT / "test-site")
+        assert src_file.read_text() == dest_file.read_text()
